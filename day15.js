379cf75cc19d19ea974708b986b9fbf9b1a974b0
Narendar <narenofficial0419@gmail.com>
2026-09-24 17:08:32 +0530

    Add frontend tasks

---
+++ day15.js
@@ -0,0 +1,37 @@
+
+    //    var a=10;
+    //    const b=20;
+        let a=30;
+        let b=20;
+    //     console.log(a)
+    //     console.log(b)
+    //     console.log(c)
+        
+
+    //             a=100;
+    // //    const b=200;
+    //      c=300;
+    //     console.log(a)
+    //     console.log(b)
+    //     console.log(c)
+
+    // console.log(`sum of  ${a} + ${b} = ${c}`);
+    //  console.log(`sum of  ${a} + ${b} = ${a+=b}`);
+    //  console.log(`sub of ${a} - ${b} = ${a-b}`);
+    //  console.log(` mul of ${a} * ${b} = ${a*b}`)
+    //  console.log(` div of ${a} / ${b} = ${a/b}`)
+    //  console.log(`modof ${a} %${b} = ${a%b}`)
+     
+     
+
+    // a=a+b;
+    // a+=b;
+    // a=a-b;
+    // a-=b
+    // a=a*b;
+    // a*=b;
+    // a=a%b;
+    // a%=b;
+    
+    console.log(`a ${a}  + b ${b} ${a++ + ++b}`);
+    
---
+++ day18.js
@@ -0,0 +1,25 @@
+for (let i=0;i<=10;i+=2)
+   {
+     console.log(i)
+   }
+   console.log("---------------------------------")
+for(let k=1; k<=10; k+=2){
+ console.log(k)
+}
+ console.log("---------------------------------")
+ let b=1;
+ while (b<10)
+ {
+ console.log(b);
+ b++;
+ }
+  console.log("---------------------------------")
+  let a=1;
+  do
+  {
+    console.log(a)
+    a+=2
+  }while (a<=15)
+
+let s=""
+for ( l=0; l<=10; 1++);
